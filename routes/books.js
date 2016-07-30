0e6ac8853cb8258b6bfeda22a4654542936b1aa5
Kieran Hegarty <kieranheg@gmail.com>
2016-07-30 23:05:31 +0100

    WIP finished GET

--- routes/books.js
+++ routes/books.js
@@ -4,12 +4,30 @@ var Book = require('../models/bookModel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Book.find(function(err, books) {
+  var query = applyQueryFilter(req.query);
+  Book.find(query, function(err, books) {
     if(err)
-      console.log(err);
+      res.status(err).send(err);
     else
       res.json(books);
   });
 });
+
+router.get('/Books/:bookId', function (req, res, next) {
+  Book.findById(req.params.bookId, function(err, book) {
+    if(err)
+      res.status(err).send(err);
+    else
+      res.json(book);
+  });
+});
+
+function applyQueryFilter(queryParam) {
+  var query = {};
+  if (queryParam.genre) {
+    query.genre = queryParam.genre;
+  }
+  return query;
+}
 
 module.exports = router;
