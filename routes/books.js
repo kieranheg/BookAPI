7c0b88c5b0ae4f80e6c2110daae6acf93b318e8e
Kieran Hegarty <kieranheg@gmail.com>
2016-08-02 22:31:57 +0100

    Added PUT and PATCH functionality

--- routes/books.js
+++ routes/books.js
@@ -4,30 +4,64 @@ var Book = require('../models/bookModel');
 
 /* GET home page. */
 router
-  .post('/', function (req, res, next) {
+  .post('/', function (req, res) {
     var book = new Book(req.body);
     book.save();
     res.status(201).send(book);
   })
-  .get('/', function (req, res, next) {
+  .get('/', function (req, res) {
     var query = applyQueryFilter(req.query);
     Book.find(query, function (err, books) {
       if (err)
-        res.status(err).send(err);
+        res.status(500).send(err);
       else
         res.json(books);
     });
   });
 
+router.use('/:bookId', function (req, res, next) {
+  Book.findById(req.params.bookId, function (err, book) {
+    if (err)
+      res.status(500).send(err);
+    else if (book) {
+      req.book = book;
+      next();
+    }
+    else {
+      res.status(404).send('No book found!');
+      ;
+    }
+  });
+});
 
 router
-  .get('/:bookId', function (req, res, next) {
-    console.log('***** querying books id ', req.params.bookId);
-    Book.findById(req.params.bookId, function (err, book) {
+  .get('/:bookId', function (req, res) {
+    res.json(req.book);
+  })
+  .put('/:bookId', function (req, res) {
+    req.book.title = req.body.title;
+    req.book.author = req.body.author;
+    req.book.genre = req.body.genre;
+    req.book.read = req.body.read;
+    req.book.save(function (err) {
       if (err)
-        res.status(err).send(err);
+        res.status(500).send(err);
       else
-        res.json(book);
+        res.json(req.book);
+    });
+  })
+  .patch('/:bookId', function (req, res) {
+    if (req.body._id) {
+      delete req.body._id;
+    }
+    for (var p in req.body) {
+      req.book[p] = req.body[p];
+    }
+    req.book.save(function (err) {
+      if (err)
+        res.status(500).send(err);
+      else
+        res.json(req.book);
     });
   });
 
