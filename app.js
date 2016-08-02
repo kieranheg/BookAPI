912252ca47a43991eb3bf6ea0f8c5ef87a4b3fab
Kieran Hegarty <kieranheg@gmail.com>
2016-08-02 21:43:35 +0100

    Added POST functionality

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var db = mongoose.connect('mongodb://localhost/bookAPI');
 var Book = require('./models/bookModel');
 
 var books = require('./routes/books');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,12 +21,11 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/Books', books);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/books.js
+++ routes/books.js
@@ -3,24 +3,33 @@ var router = express.Router();
 var Book = require('../models/bookModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var query = applyQueryFilter(req.query);
-  Book.find(query, function(err, books) {
-    if(err)
-      res.status(err).send(err);
-    else
-      res.json(books);
+router
+  .post('/', function (req, res, next) {
+    var book = new Book(req.body);
+    book.save();
+    res.status(201).send(book);
+  })
+  .get('/', function (req, res, next) {
+    var query = applyQueryFilter(req.query);
+    Book.find(query, function (err, books) {
+      if (err)
+        res.status(err).send(err);
+      else
+        res.json(books);
+    });
   });
-});
 
-router.get('/Books/:bookId', function (req, res, next) {
-  Book.findById(req.params.bookId, function(err, book) {
-    if(err)
-      res.status(err).send(err);
-    else
-      res.json(book);
+
+router
+  .get('/:bookId', function (req, res, next) {
+    console.log('***** querying books id ', req.params.bookId);
+    Book.findById(req.params.bookId, function (err, book) {
+      if (err)
+        res.status(err).send(err);
+      else
+        res.json(book);
+    });
   });
-});
 
 function applyQueryFilter(queryParam) {
   var query = {};
